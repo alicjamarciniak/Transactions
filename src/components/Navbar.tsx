61cb57654767cdd8d0066a491696aa5472f1e31b
alicjamarciniak <marciniak.al@gmail.com>
2022-10-14 17:25:38 +0200

    Adds navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <header>
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  );
+};
+
+export default Navbar;
